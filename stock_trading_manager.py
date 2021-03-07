d5fa65e0c327a59d93664f4f339456cf40765a4c
Christopher Sayon <Christopher.L.Sayon@gmail.com>
2021-03-07 19:34:39 +1100

    calculate resistance

--- stock_trading_manager.py
+++ stock_trading_manager.py
@@ -1,4 +1,20 @@
 class StockTradingManager:
-    def __init__():
+    def __init__(self):
         pass
- 
+        
+    def calculate_current_resistance(self, ask_orders):
+        """
+        Calculate current resistance by looking asks in order book
+        Orders need to be passed in a [Cryptovalue]
+        """
+        self.weight = 0
+        self.resistance_total = 0
+
+        #Calculate weighted average
+        for ask in ask_orders:
+            self.weight += ask[1]
+            self.resistance_total += ask[0] * ask[1]
+
+        self.resistance = self.resistance_total/self.weight
+
+        return self.resistance
