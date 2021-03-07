b8a2b44f0e100353eba583413aab9344599336ce
Christopher Sayon <Christopher.L.Sayon@gmail.com>
2021-03-07 18:29:52 +1100

    get candlestick data

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ binance_manager = BinanceManager(api_key, api_secret)
 binance_manager.set_coin("DOGEAUD")
 
 # get order book
-order_book = binance_manager.get_order_book(99)
-print(binance_manager.order_book_asks)
-print(binance_manager.order_book_bids)
+# order_book = binance_manager.get_order_book(99)
+# print(binance_manager.order_book_asks)
+# print(binance_manager.order_book_bids)
+
+# get candlestick data
+candlestick_data = binance_manager.get_candlestick_data(99)
+print(candlestick_data)
