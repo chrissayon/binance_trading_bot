36f118b3096949a81e32c891af66e9131b6ba2ac
Christopher Sayon <Christopher.L.Sayon@gmail.com>
2021-03-07 22:41:57 +1100

    add graph trendline

--- crypto_trading_manager.py
+++ crypto_trading_manager.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
 
 class CryptoTradingManager:
     def __init__(self):
@@ -42,8 +45,22 @@ class CryptoTradingManager:
         """
         Calulate resistance trendline by grabbing candlestick data
         """
-        # if line_type == "strength":
-        self.trend_line = np.polyfit(time_points, data_points, 1, rcond=None, full=False)
+        self.time_points = time_points
+        self.data_points = data_points
+
+        self.trend_line = np.polyfit(self.time_points, self.data_points, 1)
 
         return self.trend_line
-        
+
+    def graph_trendline(self):
+        """
+        Graphout the trendline
+        """
+        price = np.poly1d(self.trend_line)
+
+        plt.plot(self.time_points, price(self.time_points), label="trendline")
+        # plt.plot(self.time_points, self.data_points, label="high points")
+        plt.draw()
+        plt.ioff()
+        plt.title("y=%.6fx+%.6f"%(self.trend_line[0],self.trend_line[1])) 
+        plt.show()
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import logging
 from dotenv import load_dotenv
 from binance_manager import BinanceManager
+from crypto_trading_manager import CryptoTradingManager
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
@@ -12,16 +13,43 @@ api_key = os.getenv('API_KEY')
 api_secret = os.getenv('SECRET_KEY')
 
 # Initialize client
-binance_manager = BinanceManager(api_key, api_secret)
+binance_instance = BinanceManager(api_key, api_secret)
 
-# get market depth
-binance_manager.set_coin("DOGEAUD")
+# Initialize stock trading manager
+cypto_trading_instance = CryptoTradingManager()
+
+# set coin
+binance_instance.set_coin("DOGEAUD")
+
+
 
 # get order book
-# order_book = binance_manager.get_order_book(99)
-# print(binance_manager.order_book_asks)
-# print(binance_manager.order_book_bids)
+# order_book = binance_instance.get_order_book(100)
+# print(binance_instance.order_book_asks)
+# print(binance_instance.order_book_bids)
+# print(cypto_trading_instance.calculate_current_resistance(binance_instance.order_book_asks))
+# print(cypto_trading_instance.calculate_current_support(binance_instance.order_book_bids))
 
 # get candlestick data
-candlestick_data = binance_manager.get_candlestick_data(99)
-print(candlestick_data)
+# candlestick_data = binance_instance.get_candlestick_data(2)
+# print(candlestick_data)
+# print())
+
+
+
+
+# get candlestick data
+candlestick_data = binance_instance.get_candlestick_data(3)
+
+# print(binance_instance.candlestick_open_time_points)
+# print(binance_instance.candlestick_high_points)
+
+trend_line = cypto_trading_instance.calculate_trendline(
+    binance_instance.candlestick_open_time_points,
+    binance_instance.candlestick_high_points,
+)
+# print(cypto_trading_instance.trend_line)
+
+cypto_trading_instance.graph_trendline()
+
+
