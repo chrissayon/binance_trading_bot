67e7c7cb1aab8dd76eafd1e9e02749047bef635c
Christopher Sayon <Christopher.L.Sayon@gmail.com>
2021-03-07 19:43:56 +1100

    calculate current support

--- stock_trading_manager.py
+++ stock_trading_manager.py
@@ -5,7 +5,7 @@ class StockTradingManager:
     def calculate_current_resistance(self, ask_orders):
         """
         Calculate current resistance by looking asks in order book
-        Orders need to be passed in a [cryptocurrency_value, othercurrency_totalvalue]
+        Orders need to be passed in a [[cryptocurrency_value, othercurrency_totalvalue], ...] format
         """
         self.weight = 0
         self.resistance_total = 0
@@ -18,3 +18,20 @@ class StockTradingManager:
         self.resistance = self.resistance_total/self.weight
 
         return self.resistance
+
+    def calculate_current_support(self, bid_orders):
+        """
+        Calculate current support by looking bids in order book
+        Orders need to be passed in a [[cryptocurrency_value, othercurrency_totalvalue], ...] format
+        """
+        self.weight = 0
+        self.support_total = 0
+
+        #Calculate weighted average
+        for bid in bid_orders:
+            self.weight += bid[1]
+            self.support_total += bid[0] * bid[1]
+
+        self.support = self.support_total/self.weight
+
+        return self.support
