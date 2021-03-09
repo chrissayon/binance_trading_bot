a66679509c0e7152e1a0e024dbd7e07a2a7e3456
Christopher Sayon <Christopher.L.Sayon@gmail.com>
2021-03-09 17:48:02 +1100

    change src folder name

--- binance_trading_bot/main.py
+++ binance_trading_bot/main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import logging
+import constants
 from dotenv import load_dotenv
 from binance_manager import BinanceManager
 from crypto_trading_manager import CryptoTradingManager
@@ -39,27 +40,29 @@ binance_instance.set_coin("DOGEAUD")
 
 
 # get candlestick data
-candlestick_data = binance_instance.get_candlestick_data(50)
+candlestick_data = binance_instance.get_candlestick_data(14)
 
-print(binance_instance.candlestick_open_time_points)
+# print(binance_instance.candlestick_open_time_points)
 # print(binance_instance.candlestick_high_points)
 
-trend_line = cypto_trading_instance.calculate_trendline(
-    binance_instance.candlestick_open_time_points,
-    binance_instance.candlestick_high_points,
-)
+# trend_line = cypto_trading_instance.calculate_trendline(
+#     binance_instance.candlestick_open_time_points,
+#     binance_instance.candlestick_high_points,
+# )
 # print(cypto_trading_instance.trend_line)
 
-cypto_trading_instance.calculate_all_trendline(binance_instance.candlestick_dict)
+# cypto_trading_instance.calculate_all_trendline(binance_instance.candlestick_dict)
 
 # cypto_trading_instance.graph_trendline()
+
+
+cypto_trading_instance.calculate_rsi(binance_instance.candlestick_data_int)
+cypto_trading_instance.localize_rsi_to_coin(binance_instance.candlestick_data_int)
 cypto_trading_instance.graph_candlesticks(
-    binance_instance.candlestick_open_time_points,
+    # binance_instance.candlestick_open_time_points,
+    binance_instance.candlestick_dict[constants.CANDLESTICK_DATETIME_OPEN],
     binance_instance.candlestick_open_points,
     binance_instance.candlestick_high_points,
     binance_instance.candlestick_low_points,
     binance_instance.candlestick_close_points,
 )
-
-
-
